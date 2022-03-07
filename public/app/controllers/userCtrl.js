8507f0b4773a0f9b64203bbeca4f7a023f3c398e
Pankaj Tanwar Banna <2016ucp1381@mnit.ac.in>
2022-03-08 00:58:48 +0530

    feat: lottery added

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -125,7 +125,7 @@ angular
     };
   })
 
-  .controller("resultCtrl", function (user, $routeParams, $timeout) {
+  .controller("resultCtrl", function (user, $routeParams, $scope, $sce) {
     let app = this;
     user
       .purchases({ purchaseId: $routeParams.purchaseId })
@@ -137,82 +137,18 @@ angular
         app.errorMsg = error.data.response.message;
         app.loading = false;
       });
-      app.message = {};
+    app.message = {};
 
     app.spin = (index, yourCode, winnerCode) => {
       app.showSpin = {};
-      
+
       app.showSpin[index] = true;
-
-      //javascript function
-      let value1 = document.getElementById("value1");
-      let value2 = document.getElementById("value2");
-      let value3 = document.getElementById("value3");
-      let value4 = document.getElementById("value4");
-
-      let values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-      function getRandomValue() {
-        return values[Math.floor(Math.random() * 7)];
-      }
-
-      let animationId;
-
-      function updateAnimation(newSpeed) {
-        if (animationId) clearInterval(animationId);
-
-        if (newSpeed === "0") {
-          value1.innerText = getRandomValue();
-          value2.innerText = getRandomValue();
-          value3.innerText = getRandomValue();
-          value4.innerText = getRandomValue();
-
-          let speed = 5;
-
-          animationId = setInterval(() => {
-            value1.innerText = getRandomValue();
-            value2.innerText = getRandomValue();
-            value3.innerText = getRandomValue();
-            value4.innerText = getRandomValue();
-            document.documentElement.style.setProperty("--speed", speed);
-            speed = parseFloat(speed  - 0.1);
-            if (speed <= 0) {
-              value1.innerText = yourCode.toString()[0];
-              value2.innerText = yourCode.toString()[1];
-              value3.innerText = yourCode.toString()[2];
-              value4.innerText = yourCode.toString()[3];
-
-              if (yourCode.toString() == winnerCode.toString()) {
-                app.message[index] =
-                  `Yay! Winner code is ${winnerCode}, you have won the prize. Congratulations!`;
-              } else {
-                app.message[index] =
-                  `Oh, sorry! Winner code is ${winnerCode}, you could not win the prize. Better luck next time.`;
-              }
-              if (animationId) clearInterval(animationId);
-              document.documentElement.style.setProperty("--speed", 0.1);
-              $timeout(function() {
-                document.documentElement.style.setProperty("--speed", 0);
-              }, 1000)
-            }
-          }, 2000 / speed);
-        } else {
-          animationId = setInterval(() => {
-            value1.innerText = getRandomValue();
-            value2.innerText = getRandomValue();
-            value3.innerText = getRandomValue();
-            value4.innerText = getRandomValue();
-          }, 1000 / newSpeed);
-        }
-      }
-
-      document.documentElement.style.setProperty("--speed", 5);
-
-      updateAnimation(3);
-      $timeout(function () {
-        updateAnimation("0");
-      }, 2000);
     };
+
+    $scope.getIframeSrc = function (code) {
+      let val = 'https://62265c9b7d26161ddd5098ce--nifty-montalcini-2f52c6.netlify.app?answer=' + code;
+      return $sce.trustAsResourceUrl(val);
+    };    
   })
 
   .controller("myPurchasesCtrl", function (user) {
